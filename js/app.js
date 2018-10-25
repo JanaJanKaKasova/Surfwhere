920c0b977886133e713152186f7fac257f9db246
Jana Kasova <ja.kasova@seznam.cz>
2018-10-25 21:02:47 +0100

    Added function isWindy

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 // alert('Hello World');
+function isWindy(speed) {
+	if (speed > 5) {
+		alert('It is windy');
+	} else {
+		alert('It is not windy');
+	}
+}
+
+isWindy(4); 
+isWindy(10);
+isWindy(120);
+isWindy(2);
