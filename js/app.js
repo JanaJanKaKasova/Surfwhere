4c0346f54758d8265e4458c248c74987e6f7fd40
Jana Kasova <ja.kasova@seznam.cz>
2018-11-17 23:03:52 +0000

    Added classes, ids and API current weather
    Although it does not work

--- js/app.js
+++ js/app.js
@@ -1,8 +1,34 @@
 // jQuery is required to run this code
 $( document ).ready(function() {
 
+  /*let selected      = document.querySelector('.day__block--selected');
+  let days          = document.querySelectorAll('.day__block');
+  let temperature   = document.querySelector('.weather-temp--today');
+  let wind          = document.querySelector('#wind-speed');
+  let precipitation = document.querySelector('#precipitation-probality');
+  let direction     = document.querySelector('#wind-direction');
+  let description   = document.querySelector('#weather-description');
+  let place         = document.querySelector('#weather-location');
+  let icon          = document.querySelector('.weather__icon--today');
+
+  let root   = 'https://api.openweathermap.org';
+  let apiKey = '029474316bb793be56fc4dee0d85fa00';
+  let path   = 'data/2.5/weather';
+  let city   = 'Lisbon';
+
+  axios.get(root + '/' + path + '?q=' + city + '&appid=' + apiKey + '&units=metric')
+    .then(function(response) {
+        console.log(response.data);
+        temperature.innerHTML = Math.round(response.data.main.temp);
+        precipitation.innerHTML = Math.round(response.data.main.humidity);
+        place.innerHTML = String('Lisbon');
+        wind.innerHTML = Math.round(response.data.main.wind);
+        description.innerHTML = String(response.data.)
+
+    });*/
+
   // weather app code
-  let selected = document.querySelector('.day__block--selected')
+  /*let selected = document.querySelector('.day__block--selected')
   let days = document.querySelectorAll('.day__block');
   let todayTemp = document.querySelector('.weather-temp--today');
   let todayIcon = document.querySelector('.weather__icon--today');
@@ -12,13 +38,13 @@ $( document ).ready(function() {
     document.querySelector('.day__block--selected').classList.remove('day__block--selected');
     this.classList.add('day__block--selected');
 
-    let iconPath = 'images/' + this.getAttribute('conditions') + '.png';
+    let iconPath = '' + this.getAttribute('conditions');
     todayIcon.setAttribute('src', iconPath);
   };
 
   days.forEach(function(day, index) {
     day.addEventListener('click', refreshTemp);
-  });
+  });*/
 
   //video banner code
   
